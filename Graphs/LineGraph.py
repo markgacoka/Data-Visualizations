97192e7fac6910efd45694aaca950f57cea08585
Mark Gacoka Mbui <23658445+markgacoka@users.noreply.github.com>
2019-10-23 03:24:24 -0700

    Update LineGraph.py

--- Graphs/LineGraph.py
+++ Graphs/LineGraph.py
@@ -13,7 +13,14 @@ v3 = list(df['Antarctica mass (Gt)'].values.astype(float))
 plt.plot(v1, v2, label = "Greenland mass over time")
 plt.plot(v1, v3, label = "Antarctica mass over time")
 plt.xlabel('Time in years') 
-plt.ylabel('Mass in (Gt)')  
-plt.title('LAND ICE MASS DATA detailing the anomalies relative to timeseries mean') 
+plt.ylabel('Mass in (Gt)')
+plt.grid(True)
+plt.figtext(.1, -.3, "Figure 1: Annual land ice mass anomalies from the time series mean for Greenland and Antarctic Ice Sheets.\n\
+The lines indicate annual Greenland mass(Gt) anomalies and Antarctica mass(Gt) anomalies respectively over\na period of 12 years for\
+the time series mean. The graph shows a gradually decreasing positive anomaly in a \ncyclic pattern for both areas over time. The\
+two anomalies intersect around ~0 where Greenland has a faster\ndecrease towards a more negative anomaly. This indicates that there\
+is a gradual decrease in mass(Gt) having\ndecreased by an anomaly of ~3500Gt from the average in the course of 12 years presented for\
+both Greenland\nand Antarctica.")
 plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0.)
 plt.show()
+#plt.savefig('line_graph.png')
